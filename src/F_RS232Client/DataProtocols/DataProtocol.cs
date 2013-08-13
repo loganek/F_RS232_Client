cd5a0a20a8d5de3f525f6d5a9bc7ad4a2c9bc015
loganek <marcin.kolny@gmail.com>
2013-08-13 23:30:32 +0200

    added abstract dataprotocol class

---
+++ src/F_RS232Client/DataProtocols/DataProtocol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F_RS232Client.DataProtocols
+{
+    abstract class DataProtocol
+    {
+        public abstract void Open();
+        public abstract bool IsOpen();
+        public abstract void Close();
+    }
+}
