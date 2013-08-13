76260f78b217705220a26857a14139eaae926d8f
loganek <marcin.kolny@gmail.com>
2013-08-13 23:10:06 +0200

    added IWriteable interface

---
+++ src/F_RS232Client/IWriteable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F_RS232Client
+{
+    interface IWriteable
+    {
+        void Write(byte[] bytes);
+        void Write(byte b);
+        void Write(string str);
+    }
+}
