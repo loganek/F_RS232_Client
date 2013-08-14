b13e2504de2078695017a6958053222f5922fc05
loganek <marcin.kolny@gmail.com>
2013-08-14 22:03:24 +0200

    added set of plugin's interfaces

---
+++ src/F_RS232Client/PluginsInterfaces/IDataSourcePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace F_RS232Client.PluginsInterfaces
+{
+    interface IDataSourcePlugin : IPlugin
+    {
+    }
+}
