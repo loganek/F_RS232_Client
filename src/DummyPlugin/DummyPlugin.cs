5aa288ab00b49d46d320ee996bcb5f6178c1ffeb
loganek <marcin.kolny@gmail.com>
2013-08-15 11:37:27 +0200

    added first (dummy) plugin

---
+++ src/DummyPlugin/DummyPlugin.cs
@@ -0,0 +1,32 @@
+﻿using F_RS232Client.Plugins;
+
+namespace DummyPlugin
+{
+    class DummyPlugin : IPlugin
+    {
+        DummyPlugin()
+        {
+            Name = "Simple Dummy Plugin";
+            Description = "Do Nothing (or maybe says 'Hello World')";
+            Author = "Marcin Kolny <marcin.kolny@gmail.com>";
+            Version = "1.0";
+        }
+
+        #region IPlugin implementation
+        public IPluginHost Host { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Author { get; private set; }
+        public string Version { get; private set; }
+
+        public void Initialize()
+        {
+            System.Windows.Forms.MessageBox.Show("Hello World");
+        }
+
+        public void Dispose()
+        {
+        }
+        #endregion
+    }
+}
